35bd64642e9161f81ab7c50f60a3f8a161f1d639
dravis123 <randolph.ben@gmail.com>
2026-01-30 14:21:59 -0500

    framework for shooter subsystem

--- SandBot2026/src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ SandBot2026/src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -6,12 +6,16 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class SwerveSubsystem extends SubsystemBase {
-  /** Creates a new SwerveSubsystem. */
-  public SwerveSubsystem() {}
+public class SwerveSubsystem extends SubsystemBase
+{
+    /** Creates a new SwerveSubsystem. */
+    public SwerveSubsystem()
+    {
+    }
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
+    @Override
+    public void periodic()
+    {
+        // This method will be called once per scheduler run
+    }
 }
